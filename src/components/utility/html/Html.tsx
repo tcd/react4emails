95d3d8fe65f6b09f153489e09e6b646623e61035
Clay Dunston <claydunston@gmail.com>
2022-10-27 18:12:00 -0500

    misc.

--- src/components/utility/html/Html.tsx
+++ src/components/utility/html/Html.tsx
@@ -31,13 +31,13 @@ export const Html = (props: HtmlProps): JSX.Element => {
 // =============================================================================
 
 const extraProps = {
-    "dir":      "ltr",
-    "lang":     "en",
-    "xml:lang": "en",
-    "xmlns":    "http://www.w3.org/1999/xhtml",
-    // "xmlns":    "http://www.w3.org/TR/REC-html40",
-    "xmlns:m":  "http://schemas.microsoft.com/office/2004/12/omml",
-    "xmlns:o":  "urn:schemas-microsoft-com:office:office",
-    "xmlns:v":  "urn:schemas-microsoft-com:vml",
-    "xmlns:w":  "urn:schemas-microsoft-com:office:word",
+    dir:       "ltr",
+    lang:      "en",
+    xmlLang:   "en",
+    xmlns:     "http://www.w3.org/1999/xhtml",
+    // xmlns:     "http://www.w3.org/TR/REC-html40",
+    "xmlns:m": "http://schemas.microsoft.com/office/2004/12/omml",
+    "xmlns:o": "urn:schemas-microsoft-com:office:office",
+    "xmlns:v": "urn:schemas-microsoft-com:vml",
+    "xmlns:w": "urn:schemas-microsoft-com:office:word",
 }
