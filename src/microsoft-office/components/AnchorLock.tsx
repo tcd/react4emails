389d669fe691b2e584bd3ab7ae0c94bed6ebe3be
Clay Dunston <claydunston@gmail.com>
2022-10-28 10:55:14 -0500

    [tag] v0.2.1 - Add `AnchorLock` component

---
+++ src/microsoft-office/components/AnchorLock.tsx
@@ -0,0 +1,12 @@
+import { RawHTML } from "@src/components"
+
+/**
+ * `<w:anchorlock />`
+ */
+export const AnchorLock = (_props: unknown): JSX.Element => {
+    return (
+        <>
+            <RawHTML html="<w:anchorlock />"/>
+        </>
+    )
+}
